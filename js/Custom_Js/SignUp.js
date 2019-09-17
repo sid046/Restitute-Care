be59b6eeced92d5024b2edbb1988ff300d024a44
siddharth kushwaha <90siddharth@gmail.com>
2019-09-17 22:43:13 +0530

    init commi

--- js/Custom_Js/SignUp.js
+++ js/Custom_Js/SignUp.js
@@ -56,14 +56,12 @@ $("#signbtn").click(function () {
     $("#conPass-error").text("Password not matched");
   }
   else{
+    $("#signbtn").attr("disabled", true);
+    $(".spinner-border").show();
     CreateUser(email,Cpass,name,dob,phone);
   }
   
 });
-
-
-
-
 
 // function to create user 
 function CreateUser(email, Conf_Password, Name, DOB, Phone) {
@@ -80,9 +78,11 @@ function CreateUser(email, Conf_Password, Name, DOB, Phone) {
       UserType: "Customer",
     });
   }).then(function () {
-    alert("successfully Registerd.");
+    alert("User Created successfuly now you can login");
+    window.location.href = "index.html";
   }).catch(function (error) {
     var errorMessage = error.message;
-    alert(errorMessage);
+    // alert(errorMessage);
+    $('#my-error').addClass("alert alert-danger").text(errorMessage).show();
   });
 }
